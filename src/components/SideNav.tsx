39e44c20315dcf4f53993c3c7c18f51f7212b9c9
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-04-04 21:31:56 +0100

    progress: created DashboardPage, SideNav component, UsersPage

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "@/styles/sideNav.module.scss"
+
+function SideNav() {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+
+            </div>
+        </div>
+    )
+}
+
+export default SideNav
---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,22 @@
+import Header from '@/components/Header'
+import SideNav from '@/components/SideNav'
+import styles from "@/styles/dashboardPage.module.scss"
+import { Outlet } from "react-router-dom";
+
+function Dashboard() {
+    return (
+        <div className={styles.wrapper}>
+            <Header />
+            <main>
+                <section className={styles.sec1}>
+                    <SideNav />
+                </section>
+                <section className={styles.sec2}>
+                    <Outlet />
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default Dashboard
