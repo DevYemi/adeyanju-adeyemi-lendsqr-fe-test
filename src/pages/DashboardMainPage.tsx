6aa248843225bd45ebdee4968a5dc9728b362efe
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-04-05 19:06:32 +0100

    refactor: Changed routes hierarchy

---
+++ src/pages/DashboardMainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashboardMainPage() {
+    return (
+        <div>
+            This is the Dashboard Page route, Click on the Users Link on the side bar to see the rest of the design
+        </div>
+    )
+}
+
+export default DashboardMainPage
--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import LoadingSpinner from '@/components/LoadingSpinner';
 import DashboardPage from "@/pages/DashboardPage";
 const ErrorPage = lazy(() => import('@/pages/ErrorPage'));
-// const DashboardPage = lazy(() => import("@/pages/DashboardPage"));
+const DashboardMainPage = lazy(() => import("@/pages/DashboardMainPage"));
 const UsersPage = lazy(() => import("@/pages/UsersPage"));
 const LoginPage = lazy(() => import("@/pages/LoginPage"))
 
@@ -41,6 +41,19 @@ export default function GetRoutes() {
             >
                 <Route
                     index={true}
+                    element={
+                        <Suspense fallback={<LoadingSpinner />}>
+                            <DashboardMainPage />
+                        </Suspense>
+                    }
+                    errorElement={
+                        <Suspense fallback={<LoadingSpinner />}>
+                            <ErrorPage />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path='/users'
                     element={
                         <Suspense fallback={<LoadingSpinner />}>
                             <UsersPage />
