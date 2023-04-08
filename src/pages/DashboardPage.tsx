e2c3f425771d196de69f5d1a3882891b492f6423
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-04-08 13:05:08 +0100

    fix: implemented scroll restoration on page route

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,12 +2,13 @@ import Header from '@/components/Header'
 import SideNav from '@/components/SideNav'
 import { useAppSelector } from '@/redux-toolkit/hooks';
 import styles from "@/styles/dashboardPage.module.scss"
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 function Dashboard() {
     const { isOpen: isSideNavOpen } = useAppSelector(state => state.sideNavVisibility);
     return (
         <div className={styles.wrapper}>
+            <ScrollRestoration />
             <Header />
             <main>
                 <section className={`${styles.sec1} ${isSideNavOpen ? styles.sec1Open : ""}`}>
