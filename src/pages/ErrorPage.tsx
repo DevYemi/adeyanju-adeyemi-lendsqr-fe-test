60fc11ed194d374b4d55cdfffa2b382238f82ea1
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-04-09 23:30:02 +0100

    refactor: Added more context and styling to ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useRouteError } from "react-router-dom";
+import styles from "@/styles/errorPage.module.scss"
 
-function ErrorPage() {
+export default function ErrorPage() {
+    const error: any = useRouteError();
+
     return (
-        <div>ErrorPage</div>
-    )
-}
 
-export default ErrorPage
+        <div className={styles.container}>
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error?.statusText || error.message}</i>
+            </p>
+        </div>
+    );
+}
